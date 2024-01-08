2586254191a392c0eb92d7bf0e39b83ca4626164
Doichin251002 <dodi_hristov@abv.bg>
2024-01-08 23:10:10 +0200

    Controller Class Contact in Phonebook Project

--- Basic Web Project/phonebook/src/main/java/phonebook/controller/ContactController.java
+++ Basic Web Project/phonebook/src/main/java/phonebook/controller/ContactController.java
@@ -2,29 +2,49 @@ package phonebook.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.servlet.ModelAndView;
 import phonebook.entity.Contact;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@Controller("/")
 public class ContactController {
     private List<Contact> contacts;
 
     public ContactController() {
         this.contacts = new ArrayList<>();
-        this.contacts.add(new Contact("Nick", "000000000000"));
-        this.contacts.add(new Contact("John", "111111111111"));
-        this.contacts.add(new Contact("Mason", "22222222222"));
     }
 
-    @GetMapping("/")
+    @GetMapping
     public ModelAndView index(ModelAndView modelAndView) {
         modelAndView.setViewName("index");
 
         modelAndView.addObject("contacts", this.contacts);
 
         return modelAndView;
+    }
+
+    @PostMapping
+    public String add(Contact contact) {
+        this.contacts.add(contact);
+
+        return "redirect:/";
+    }
+
+    @PutMapping
+    public String updateByName(Contact changedContact) {
+        for (Contact contact : this.contacts) {
+            if (contact.getName().equals(changedContact.getName())) {
+                this.contacts.remove(contact);
+
+                this.contacts.add(changedContact);
+                break;
+            }
+        }
+
+        return "redirect:/";
     }
 }
