fe72907f3c766fdedaf37fa38ea768747c403f6c
Doichin251002 <dodi_hristov@abv.bg>
2024-01-08 22:15:43 +0200

    Entity Class Contact in Phonebook Project

--- Basic Web Project/phonebook/src/main/java/phonebook/entity/Contact.java
+++ Basic Web Project/phonebook/src/main/java/phonebook/entity/Contact.java
@@ -1,29 +1,15 @@
 package phonebook.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Contact {
     private String name;
     private String number;
-
-    public Contact () {}
-
-    public Contact (String name, String number) {
-        setName(name);
-        setNumber(number);
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getNumber() {
-        return this.number;
-    }
-
-    private void setName(String name) {
-        this.name = name;
-    }
-
-    private void setNumber(String number) {
-        this.number = number;
-    }
 }
